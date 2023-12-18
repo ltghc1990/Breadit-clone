9d3ab2cf889f38988909b4d215f1d1a5586281cd
Howell <ltghc1990@gmail.com>
2023-12-18 17:11:25 -0500

    cleared up type error in core.ts

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,17 +1,16 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { getToken } from "next-auth/jwt";
 
 const f = createUploadthing();
-
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
+    .middleware(async (req) => {
       // This code runs on your server before upload
-      const user = await auth(req);
+      const user = await getToken({ req });
 
       // If you throw, the user will not be able to upload
       if (!user) throw new Error("Unauthorized");
@@ -21,9 +20,6 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
-
-      console.log("file url", file.url);
     }),
 } satisfies FileRouter;
 
